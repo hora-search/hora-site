c2a3740f30be87339d569703f206b153a6f53f7b
salamer <salamer_gaga@163.com>
2021-07-11 18:57:50 +0800

    update: update docs

--- home/pages/index.tsx
+++ home/pages/index.tsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import Script from 'next/script';
+import Head from 'next/head'
 
 import NavBar from '../components/NavBar';
 import BigTitle from '../components/BigTitle';
@@ -13,9 +14,21 @@ import Installation from '../components/Installation';
 
 import c from '../styles/home.module.scss';
 
+const Header = (): JSX.Element => {
+  return (
+    <Head>
+      <title>Hora Search Everywhere</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"></meta>
+      <meta data-react-helmet="true" name="keywords" content="Hora, Approximate Nearest Neighbor Search"></meta>
+    </Head>
+  )
+}
+
 const Home = (): JSX.Element => {
   return (
     <div>
+      <Header />
       <NavBar />
       <div className={c.wrapper}>
         <BigTitle />
