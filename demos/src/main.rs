5e28078d683fb0d0631e1c99e9b3ad2d1e03b339
salamer <salamer_gaga@163.com>
2021-07-16 23:53:26 +0800

    update: update docs

--- demos/src/main.rs
+++ demos/src/main.rs
@@ -1,17 +1,26 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use csv;
 use futures::future::{ready, Ready};
 use hora;
 use hora::core::ann_index::ANNIndex;
 use rand::distributions::{Alphanumeric, Standard, Uniform};
 use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
+use serde_json::Result as OtherResult;
 use std::collections::HashMap;
+use std::fs;
 use std::fs::File;
 use std::io::Read;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct WineReviewsSearchItem {
     embedding: Vec<f32>,
+    id: String,
+    country: String,
+    description: String,
+    designation: String,
+    province: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -44,19 +53,20 @@ struct PicItem {
     embedding: Vec<f32>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct WineReviewsItem {
     id: String,
     country: String,
     description: String,
     designation: String,
     province: String,
-    embedding: Vec<f32>,
+    embedding: String,
 }
 
 struct ServingData {
     indices: HashMap<String, Box<hora::index::hnsw_idx::HNSWIndex<f32, String>>>,
 
-    wine_reviews_data: HashMap<String, WineReviewsItem>,
+    wine_reviews_data: HashMap<String, WineReviewsSearchItem>,
     wine_reviews_key_list: Vec<String>,
     celebrity_data: HashMap<String, PicItem>,
     celebrity_key_list: Vec<String>,
@@ -65,16 +75,28 @@ struct ServingData {
 }
 
 fn prepare_data() -> web::Data<ServingData> {
+    const wine: &str = "wine";
+    const celebrity: &str = "celebrity";
+
     let mut indices: HashMap<String, Box<hora::index::hnsw_idx::HNSWIndex<f32, String>>> =
         HashMap::new();
     indices.insert(
-        "celebrity".to_string(),
+        celebrity.to_string(),
         Box::new(hora::index::hnsw_idx::HNSWIndex::<f32, String>::new(
             128,
             &hora::index::hnsw_params::HNSWParams::<f32>::default(),
         )),
     );
 
+    indices.insert(
+        wine.to_string(),
+        Box::new(hora::index::hnsw_idx::HNSWIndex::<f32, String>::new(
+            768,
+            &hora::index::hnsw_params::HNSWParams::<f32>::default(),
+        )),
+    );
+
+    // celebrity
     let mut file = File::open("celebrity_emebeddings.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
@@ -95,21 +117,59 @@ fn prepare_data() -> web::Data<ServingData> {
         );
         celebrity_key_list.push(x.pic_name.clone());
         indices
-            .get_mut("celebrity")
+            .get_mut(celebrity)
             .unwrap()
             .add(&x.embedding, x.pic_name.clone());
     });
     println!("start build {:?} point", celebrity_key_list.len());
     indices
-        .get_mut("celebrity")
+        .get_mut(celebrity)
+        .unwrap()
+        .build(hora::core::metrics::Metric::Euclidean);
+
+    // wine
+    let filename = "winemag-data_first150k_processed.csv";
+    let wine_file = File::open(filename).unwrap();
+    let mut rdr = csv::Reader::from_reader(wine_file);
+    let mut wine_reviews_key_list = Vec::new();
+    let mut wine_data: HashMap<String, WineReviewsSearchItem> = HashMap::new();
+    for result in rdr.deserialize() {
+        let record: WineReviewsItem = result.unwrap();
+        if record.embedding.len() == 0 {
+            continue;
+        }
+        let temp: Vec<f32> = serde_json::from_str(&record.embedding).unwrap();
+        let id = record.id.clone();
+        indices
+            .get_mut(wine)
+            .unwrap()
+            .add(&temp.clone(), id.clone());
+
+        wine_data.insert(
+            record.id.clone(),
+            WineReviewsSearchItem {
+                embedding: temp,
+                id: record.id.clone(),
+                country: record.country,
+                description: record.description,
+                designation: record.designation,
+                province: record.province,
+            },
+        );
+        wine_reviews_key_list.push(id);
+    }
+    println!("start build {:?} point", wine_reviews_key_list.len());
+
+    indices
+        .get_mut(wine)
         .unwrap()
         .build(hora::core::metrics::Metric::Euclidean);
 
     println!("finish preparing");
     web::Data::new(ServingData {
         indices: indices,
-        wine_reviews_data: HashMap::new(),
-        wine_reviews_key_list: Vec::new(),
+        wine_reviews_data: wine_data,
+        wine_reviews_key_list: wine_reviews_key_list.clone(),
         celebrity_data: celebrity_data,
         celebrity_key_list: celebrity_key_list.clone(),
         cats_data: HashMap::new(),
