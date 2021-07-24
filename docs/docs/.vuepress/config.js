d549b0f289ed1b0fc56750e30e8a9df091833270
Mongkii <mongkii@hotmail.com>
2021-07-25 01:21:21 +0800

    fix: try fix header redirect to 404

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,21 +1,34 @@
-const { fs, path } = require('@vuepress/shared-utils')
+const { fs, path } = require('@vuepress/shared-utils');
 
-module.exports = ctx => ({
+module.exports = (ctx) => ({
   dest: '../../vuepress',
   locales: {
     '/': {
       lang: 'en-US',
       title: 'Hora Search',
-      description: 'Hora Search Everywhere, a light approximate nearest neighbor search library in rust'
-    }
+      description:
+        'Hora Search Everywhere, a light approximate nearest neighbor search library in rust',
+    },
   },
   head: [
-    ['script', {async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-ZCQDT118MM"}],
-    ['script', {}, `window.dataLayer = window.dataLayer || [];
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-ZCQDT118MM' }],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
   
-    gtag('config', 'G-ZCQDT118MM');`],
+    gtag('config', 'G-ZCQDT118MM');`,
+    ],
+    [
+      'script',
+      { defer: true },
+      `var header = document.querySelector('.home-link');
+    if (header) {
+      header.href = 'https://horasearch.com/';
+    }`,
+    ],
     ['link', { rel: 'icon', href: `/logo.png` }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
@@ -24,7 +37,7 @@ module.exports = ctx => ({
     ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ],
   theme: '@vuepress/vue',
   themeConfig: {
@@ -43,9 +56,9 @@ module.exports = ctx => ({
         nav: require('./nav/en'),
         sidebar: {
           '/doc/': getGuideSidebar('Basic', 'Indexes'),
-        }
-      }
-    }
+        },
+      },
+    },
   },
   plugins: [
     ['@vuepress/back-to-top', true],
@@ -58,53 +71,52 @@ module.exports = ctx => ({
         },
       },
     ],
-    ['@vuepress/pwa', {
-      serviceWorker: true,
-      updatePopup: true
-    }],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true,
+      },
+    ],
     ['@vuepress/medium-zoom', true],
-    ['@vuepress/google-analytics', {
-      ga: 'UA-128189152-1'
-    }],
-    ['container', {
-      type: 'vue',
-      before: '<pre class="vue-container"><code>',
-      after: '</code></pre>'
-    }],
-    ['container', {
-      type: 'upgrade',
-      before: info => `<UpgradePath title="${info}">`,
-      after: '</UpgradePath>'
-    }],
-    ['flowchart']
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-128189152-1',
+      },
+    ],
+    [
+      'container',
+      {
+        type: 'vue',
+        before: '<pre class="vue-container"><code>',
+        after: '</code></pre>',
+      },
+    ],
+    [
+      'container',
+      {
+        type: 'upgrade',
+        before: (info) => `<UpgradePath title="${info}">`,
+        after: '</UpgradePath>',
+      },
+    ],
+    ['flowchart'],
   ],
-  extraWatchFiles: [
-    '.vuepress/nav/en.js',
-  ]
-})
+  extraWatchFiles: ['.vuepress/nav/en.js'],
+});
 
-function getGuideSidebar (groupA, groupB) {
+function getGuideSidebar(groupA, groupB) {
   return [
     {
       title: groupA,
       collapsable: false,
-      children: [
-        '',
-        'install',
-        'benchmark',
-        'example'
-      ]
+      children: ['', 'install', 'benchmark', 'example'],
     },
     {
       title: groupB,
       collapsable: false,
-      children: [
-        'BruteForceIndex',
-        'HNSWIndex',
-        'SSGIndex',
-        'PQIndex',
-        'PQIVFIndex',
-      ]
-    }
-  ]
+      children: ['BruteForceIndex', 'HNSWIndex', 'SSGIndex', 'PQIndex', 'PQIVFIndex'],
+    },
+  ];
 }
